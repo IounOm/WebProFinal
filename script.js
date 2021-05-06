2043e5160b6cf8ab84ea5be8ecc69997be78384f
IounOm <iounnirach15@gmail.com>
2021-05-06 23:28:42 +0700

    Delete remain from script.js & add from action in register.html

--- script.js
+++ script.js
@@ -21,21 +21,18 @@ function showData(data){
         var wood = document.createElement("p");
         var price = document.createElement("p");
         var detail = document.createElement("p");
-        var remain = document.createElement("p");
 
         furniture_name.innerHTML = "Name : " + data[keys[i]].brandname;
         size.innerHTML = "Size : " + data[keys[i]].size;
         wood.innerHTML = "Wood : " + data[keys[i]].wood;
         price.innerHTML = "Price : " + data[keys[i]].price;
         detail.innerHTML = "Detail : " + data[keys[i]].detail;
-        remain.innerHTML = "Remain : " + data[keys[i]].remain;
         
         container.appendChild(furniture_name);
         container.appendChild(size);
         container.appendChild(wood);
         container.appendChild(price);
         container.appendChild(detail);
-        container.appendChild(remain);
 
         showIDlayer.appendChild(container);
     }
